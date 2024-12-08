0719398e397b9974d69ea09c2ec4ba20a64c130a
CristianMastro <cristian.mastro92@outlook.it>
2024-12-08 10:20:36 +0100

    feat;: add euro

--- script.js
+++ script.js
@@ -9,23 +9,33 @@ if (isNaN(quantiAnniHai) || isNaN(chilometriViaggio)) {
 } else if (Number(quantiAnniHai)<=0 || Number(chilometriViaggio)<=0) {
     console.log(`Inserisci un valore maggiore di 0`);
 
-    //FUNZIONE PER CALCOLO BIGLIETTI MINORENNI//
+//COSTO BIGLIETTI UNDER 18//
+
 } else if (Number(quantiAnniHai)<18) {
     function sottraiPercentuale(chilometriViaggio, scontoMinorenni) {
-       return chilometriViaggio - (chilometriViaggio * scontoMinorenni) / 100; 
+        return chilometriViaggio - (chilometriViaggio * scontoMinorenni) / 100; 
     }
-        let scontoMinorenni = 20;
-        let prezzoMinorenni = sottraiPercentuale((chilometriViaggio * 0.21),scontoMinorenni);
-        console.log(`Il tuo viaggio costa il 20% in meno`, prezzoMinorenni);
-    //FUNZIONE PER CALCOLO BIGLIETTI OVER 65// 
+    const scontoMinorenni = 20;
+    let prezzoMinorenni = sottraiPercentuale((chilometriViaggio * 0.21),scontoMinorenni);
+    let prezzoInEuro = `€${prezzoMinorenni.toFixed(2).replace(".",",")}`
+    console.log(`Il tuo viaggio costa il 20% in meno`, prezzoInEuro);
+
+//COSTO BIGLIETTI OVER 65//
+
 } else if (Number(quantiAnniHai)>65) {
     function sottraiPercentualeOver(chilometriViaggio, scontoOver) {
-       return chilometriViaggio - (chilometriViaggio * scontoOver) / 100; 
-    }
-        let scontoOver = 40;
-        let prezzoOver = sottraiPercentualeOver((chilometriViaggio * 0.21),scontoOver);
-        console.log(`Il tuo viaggio costa il 40% in meno`, prezzoOver);
+        return chilometriViaggio - (chilometriViaggio * scontoOver) / 100; 
+    }    
+    const scontoOver = 40;
+    let prezzoOver = sottraiPercentualeOver((chilometriViaggio * 0.21),scontoOver);
+    let prezzoInEuro = `€${prezzoOver.toFixed(2).replace(".",",")}`
+    console.log(`Il tuo viaggio costa il 40% in meno`, prezzoInEuro);
+
+//COSTO BIGLIETTI NORMALI//
+    
 } else {
     quantiAnniHai = Number(quantiAnniHai)
-    console.log(`Il tuo viaggio costa`, Number(chilometriViaggio)*0.21);
+    let prezzoNormale = Number(chilometriViaggio)*0.21
+    let prezzoInEuro = `€${prezzoNormale.toFixed(2).replace(".",",")}`
+    console.log(`Il tuo viaggio costa`, prezzoInEuro);
 }
